fec27811a288be32b165775dec71cfbd917b75ba
jovid_18 <cshcho99@gmail.com>
2024-03-16 17:12:32 +0900

    FIX: fix typo

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,35 +5,28 @@ import { useNavigate } from 'react-router-dom';
 function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // useNavigate hook 추가
 
   const handleSubmit = async (event) => {
+    // async 키워드 추가
     event.preventDefault();
     try {
-      // 여기에 "가벼운" 쿠키를 생성하는 코드를 추가합니다.
-      document.cookie = 'lightweightCookie=exampleValue; path=/;';
-
-      // 로그인 요청을 서버에 보냅니다. withCredentials 옵션을 true로 설정하여 쿠키를 포함시킵니다.
-      const response = await axios.post(
-        'http://localhost:3000/api/login',
-        {
-          email,
-          password,
-        },
-        {
-          withCredentials: true,
-        }
-      );
+      // 로그인 요청을 서버에 보냅니다.
+      const response = await axios.post('http://localhost:3000/api/login', {
+        email,
+        password,
+      }); // withCredentials 옵션을 true로 설정
 
       console.log(email);
       console.log(password);
       console.log(response.data); // 응답 로그 확인
 
       // 로그인 성공 시 다음 페이지로 이동
-      navigate('/'); // 성공 시 리디렉션할 페이지 주소를 적절히 조정하세요.
+      navigate('/'); // '/main' 페이지로 이동
     } catch (error) {
       console.error('로그인 오류:', error);
-      alert('로그인 실패'); // 오류 메시지를 개선하였습니다.
+      alert(email, password);
+      console.log(email);
     }
   };
 
@@ -54,41 +47,45 @@ function LoginPage() {
         }}
       >
         <h1 style={{ textAlign: 'center' }}>로그인</h1>
-        <form
-          onSubmit={handleSubmit}
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            border: 'none',
-            boxShadow: 'none',
-            textAlign: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email" style={{ margin: '0.5rem 0', padding: '0.5rem' }} />
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="비밀번호" style={{ margin: '0.5rem 0', padding: '0.5rem' }} />
-          <button
-            type="submit"
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          {' '}
+          {/* 폼 컨테이너 추가 */}
+          <form
+            onSubmit={handleSubmit}
             style={{
-              padding: '0.5rem',
-              marginTop: '1rem',
-              backgroundColor: '#B1BDC5',
-              color: 'white',
-              transition: 'background-color 0.3s',
+              display: 'flex',
+              flexDirection: 'column',
               border: 'none',
-              borderRadius: '5px',
-              cursor: 'pointer',
-            }}
-            onMouseEnter={(e) => {
-              e.target.style.backgroundColor = '#92A4AD';
-            }}
-            onMouseLeave={(e) => {
-              e.target.style.backgroundColor = '#B1BDC5';
+              boxShadow: 'none',
+              textAlign: 'center',
+              alignItems: 'center', // input 요소를 수직 가운데 정렬
             }}
           >
-            로그인
-          </button>
-        </form>
+            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email" style={{ margin: '0.5rem 0', padding: '0.5rem' }} />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="비밀번호" style={{ margin: '0.5rem 0', padding: '0.5rem' }} />
+            <button
+              type="submit"
+              style={{
+                padding: '0.5rem',
+                marginTop: '1rem',
+                backgroundColor: '#B1BDC5',
+                color: 'white',
+                transition: 'background-color 0.3s', // 배경색 변경 시 부드러운 전환 효과를 추가
+                border: 'none',
+                borderRadius: '5px',
+                cursor: 'pointer',
+              }}
+              onMouseEnter={(e) => {
+                e.target.style.backgroundColor = '#92A4AD';
+              }} // 호버 시 배경색 변경
+              onMouseLeave={(e) => {
+                e.target.style.backgroundColor = '#B1BDC5';
+              }} // 호버 종료 시 배경색 원래대로
+            >
+              로그인
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
